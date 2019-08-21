662d7c1d65bdfcf52305a78c1a5d30496e22e384
Joao Thompson <joaopthompson@gmail.com>
2019-08-21 14:48:54 -0300

    raked

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '12344555',
+    category:     'indian',
+    reviews:      @review = Review.new(content: "good indian restaurant in Shoreditch", rating: 5)
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '23493843',
+    category:     'italian',
+    reviews:      @review = Review.new(content: "decent pizza restaurant in Shoreditch", rating: 3)
+  },
+  {
+    name:         'Bullguer',
+    address:      'Rua Fradique Coutinho',
+    phone_number: '23423423',
+    category:     'american',
+    reviews:      @review = Review.new(content: "good burgers", rating: 5)
+  },
+  {
+    name:         'Vintao',
+    address:      'Rua Fidalga',
+    phone_number: '4354344',
+    category:     'brazilian',
+    reviews:      @review = Review.new(content: "good price, ok food", rating: 3)
+  },
+  {
+    name:         'Astor',
+    address:      'Vila Madalena',
+    phone_number: '83489549',
+    category:     'brazilian',
+    reviews:      @review = Review.new(content: "great beer, good food", rating: 4)
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
